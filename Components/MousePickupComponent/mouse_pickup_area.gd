e2c0443a6dabfa4d88382dd253e4a0c2cac64899
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-11 15:06:07 -0500

    formatting

--- Components/MousePickupComponent/mouse_pickup_area.gd
+++ Components/MousePickupComponent/mouse_pickup_area.gd
@@ -4,12 +4,14 @@ extends Area2D
 @export var progress_bar: ProgressBar
 @export var delete_node: Node
 @export var item: ItemData
+@export var time := .3
 
 @onready var timer = $Timer
 
 signal picked_up(item: ItemData)
 
 func _ready():
+	timer.wait_time = time
 	progress_bar.hide()
 	progress_bar.min_value = 0
 	progress_bar.max_value = timer.wait_time
