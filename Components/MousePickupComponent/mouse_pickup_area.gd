723896d03a6f0c33753ac7c9f4bbe6fe52337305
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-11 22:30:01 -0500

    pickup handler calls pickup out of range on pickup area and pickup area spawns text when out of range

--- Components/MousePickupComponent/mouse_pickup_area.gd
+++ Components/MousePickupComponent/mouse_pickup_area.gd
@@ -6,6 +6,7 @@ extends Area2D
 @export var item: ItemData
 @export var time := .25
 
+@onready var text_spawner: FloatAwayTextSpawner = $FloatAwayTextSpawner
 @onready var timer = $Timer
 
 signal picked_up(item: ItemData)
@@ -34,6 +35,9 @@ func _input(event):
 func pickup():
 	delete_node.queue_free()
 	picked_up.emit(item)
+
+func pickup_out_of_range():
+	text_spawner.spawn_text()
 
 func _on_timer_timeout():
 	pickup()
