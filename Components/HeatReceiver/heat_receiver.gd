4da96c24d2dc98d3c8e25d1f619414a71039ad07
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-15 16:19:30 -0500

    laser has a line

--- Components/HeatReceiver/heat_receiver.gd
+++ Components/HeatReceiver/heat_receiver.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 var receiving_heat = 0
 
-signal just_received_damage(damage: int)
+signal new_heat_area(heat_area: HeatArea)
 
 func _physics_process(delta):
 	var heat_areas_overlapping: Array[Area2D] = get_overlapping_areas().filter(func(a: Area2D): return a is HeatArea)
@@ -11,4 +11,4 @@ func _physics_process(delta):
 
 func _on_area_entered(area):
 	if area is HeatArea:
-		just_received_damage.emit(area.damage_amount)
+		new_heat_area.emit(area)
--- Objects/Laser/laser.gd
+++ Objects/Laser/laser.gd
@@ -4,6 +4,7 @@ const SHAPE_OFFSET = 2.5
 
 @onready var raycast: RayCast2D = $RayCast2D
 @onready var collision_shape: CollisionShape2D = $DamageArea/CollisionShape2D
+@onready var line: Line2D = $Line2D
 
 @export var maximum_length := 750
 
@@ -11,4 +12,6 @@ func _ready():
 	raycast.target_position.x = maximum_length
 
 func _physics_process(delta):
-	collision_shape.global_position = raycast.get_collision_point() if raycast.is_colliding() else raycast.target_position + raycast.global_position
+	var laser_end_position: Vector2 = raycast.get_collision_point() if raycast.is_colliding() else raycast.target_position + raycast.global_position
+	collision_shape.global_position = laser_end_position
+	line.set_point_position(1, laser_end_position - line.global_position)
