7471fa76eff54eb72d6ba1f8a87d543544f1d4ee
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-15 14:12:11 -0500

    laser

--- Objects/Laser/laser.gd
+++ Objects/Laser/laser.gd
@@ -5,8 +5,13 @@ const SHAPE_OFFSET = 2.5
 @onready var raycast: RayCast2D = $RayCast2D
 @onready var collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
 
+@export var maximum_length := 750
+
+func _ready():
+	raycast.target_position.x = maximum_length
+
 func _physics_process(delta):
-	var length = (raycast.get_collision_point() - raycast.get_global_transform().origin).length() if raycast.is_colliding() else raycast.target_position.y
+	var length = (raycast.get_collision_point() - raycast.get_global_transform().origin).length() if raycast.is_colliding() else raycast.target_position.x
 	#doing times 3 so the collision shape goes into the area a little bit so it can detect it.
-	collision_shape.shape.size.y = length + (SHAPE_OFFSET * 3)
-	collision_shape.position.y = length / 2 - SHAPE_OFFSET
+	collision_shape.shape.size.x = length + (SHAPE_OFFSET * 3)
+	collision_shape.position.x = length / 2 - SHAPE_OFFSET
