a1d04bb5aace9a1ace5d9c8b45661cc61094d042
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-15 14:19:09 -0500

    collision isnt across the whole laser, only at end

--- Objects/Laser/laser.gd
+++ Objects/Laser/laser.gd
@@ -3,7 +3,7 @@ extends Node2D
 const SHAPE_OFFSET = 2.5
 
 @onready var raycast: RayCast2D = $RayCast2D
-@onready var collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var collision_shape: CollisionShape2D = $DamageArea/CollisionShape2D
 
 @export var maximum_length := 750
 
@@ -11,7 +11,4 @@ func _ready():
 	raycast.target_position.x = maximum_length
 
 func _physics_process(delta):
-	var length = (raycast.get_collision_point() - raycast.get_global_transform().origin).length() if raycast.is_colliding() else raycast.target_position.x
-	#doing times 3 so the collision shape goes into the area a little bit so it can detect it.
-	collision_shape.shape.size.x = length + (SHAPE_OFFSET * 3)
-	collision_shape.position.x = length / 2 - SHAPE_OFFSET
+	collision_shape.global_position = raycast.get_collision_point() if raycast.is_colliding() else raycast.target_position + raycast.global_position
