2b462dab5070b136584fe28d2dacc64145d98aff
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-16 23:04:49 -0500

    overheatable wall changes color to indicate if its on

--- Objects/OverheatingWall/overheatable_wall.gd
+++ Objects/OverheatingWall/overheatable_wall.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+const FULL_HEAT_COLOR = Color(1,0,0)
+const INITIAL_COLOR = Color(0.576, 0.373, 1)
+
+@onready var color_rect = $ColorRect
+@onready var overheater = $Overheater
+
 func _on_overheater_max_heat_reached():
 	queue_free()
+
+func _process(delta):
+	color_rect.color = INITIAL_COLOR.lerp(FULL_HEAT_COLOR, remap(overheater.heat, 0, overheater.max_heat, 0, 1))
