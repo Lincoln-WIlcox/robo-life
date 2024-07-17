32f4d8bba4974004337aa127de92b4de8e6ee39b
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-16 22:41:04 -0500

    renaming variable to be heat

--- Components/Overheater/overheater.gd
+++ Components/Overheater/overheater.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var damage_receiver = $DamageReceiver
+@onready var heat_receiver = $DamageReceiver
 
 @export var max_heat := 1000
 @export var cooldown_rate := 1
@@ -9,6 +9,12 @@ var heat = 0
 
 signal max_heat_reached
 
+func _ready():
+	var collision_shapes: Array[Node2D] = get_children().filter(func(c: Node): return c is CollisionShape2D)
+	for collision_shape: CollisionShape2D in collision_shapes:
+		remove_child(collision_shape)
+		heat_receiver.add_child(collision_shape)
+
 func _physics_process(delta):
-	heat += damage_receiver.receiving_damage if damage_receiver.receiving_damage else -cooldown_rate
+	heat += heat_receiver.receiving_damage if heat_receiver.receiving_damage else -cooldown_rate
 	heat = max(heat, 0)
