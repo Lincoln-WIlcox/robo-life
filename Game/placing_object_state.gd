8af0e547c8ec92216f3788d76b5a9145f763c0ce
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-17 17:43:58 -0500

    item gets removed on place

--- Game/placing_object_state.gd
+++ Game/placing_object_state.gd
@@ -6,8 +6,7 @@ extends State
 var placeable_item: PlaceableItemData
 var place_object: Callable
 var cancel_placing_object: Callable
-
-signal item_placed(item)
+var inventory: Inventory
 
 func enter():
 	place_object_handler.start_placing_placeable(load(placeable_item.drop_scene).instantiate())
@@ -15,7 +14,7 @@ func enter():
 func run():
 	place_object_handler.update_placing()
 	if place_object.call() and place_object_handler.attempt_place_object():
-		item_placed.emit(placeable_item)
+		inventory.remove_item(placeable_item)
 		state_ended.emit(none_state)
 	if cancel_placing_object.call():
 		place_object_handler.cancel_placing()
