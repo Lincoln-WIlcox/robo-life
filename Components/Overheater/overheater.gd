d81dcac999da63784eab568bf4ee21a998d26f0b
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-16 22:52:53 -0500

    overheatable wall works

--- Components/Overheater/overheater.gd
+++ Components/Overheater/overheater.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var heat_receiver = $DamageReceiver
+@onready var heat_receiver = $HeatReceiver
 
 @export var max_heat := 1000
 @export var cooldown_rate := 1
@@ -15,13 +15,13 @@ var overheated := false:
 signal max_heat_reached
 
 func _ready():
-	var collision_shapes: Array[Node2D] = get_children().filter(func(c: Node): return c is CollisionShape2D)
+	var collision_shapes: Array[Node] = get_children().filter(func(c: Node): return c is CollisionShape2D)
 	for collision_shape: CollisionShape2D in collision_shapes:
 		remove_child(collision_shape)
 		heat_receiver.add_child(collision_shape)
 
 func _physics_process(delta):
-	heat += heat_receiver.receiving_damage if heat_receiver.receiving_damage else -cooldown_rate
+	heat += heat_receiver.receiving_heat if heat_receiver.receiving_heat else -cooldown_rate
 	heat = max(heat, 0)
 	
 	overheated = heat >= max_heat
