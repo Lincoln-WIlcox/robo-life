3f0b9569afa884f7c97500fc27a1279a40293ffe
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-19 16:13:15 -0500

    enemy only shoots if player is in line of sight

--- Characters/Enemies/BasicEnemy/shoot_player_controller.gd
+++ Characters/Enemies/BasicEnemy/shoot_player_controller.gd
@@ -4,12 +4,17 @@ extends Node
 
 @export var bullet_packed_scene: PackedScene
 @export var character: CharacterBody2D
+@export var line_of_sight_of_target: RayCast2D
 
 var node_to_put_bullets_in: Node
 var target := Vector2.ZERO
 
 func _physics_process(delta):
-	print(target)
+	line_of_sight_of_target.target_position = target
+	if line_of_sight_of_target.is_colliding() and not timer.is_stopped():
+		timer.stop()
+	elif not line_of_sight_of_target.is_colliding() and timer.is_stopped():
+		timer.start()
 
 func _on_timer_timeout():
 	_shoot_bullet()
