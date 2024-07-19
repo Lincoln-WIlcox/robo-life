17710b268c46e31b71a252ebee90354ae0419432
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-19 13:34:45 -0500

    changes

--- Objects/Bullet/bullet.gd
+++ Objects/Bullet/bullet.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var hitbox = $Hitbox
-@onready var visible_on_screen_notifier = $Hitbox/VisibleOnScreenNotifier2D
+@onready var marker = $Marker2D
+@onready var visible_on_screen_notifier = $Marker2D/VisibleOnScreenNotifier2D
 
 @export var speed := 18
 @export var direction := 0:
@@ -10,12 +10,12 @@ extends Node2D
 	get:
 		return rotation_degrees
 
-#func _ready():
-	#if not visible_on_screen_notifier.is_on_screen():
-		#queue_free()
+func _ready():
+	if not visible_on_screen_notifier.is_on_screen():
+		destroy()
 
 func _physics_process(delta):
-	hitbox.position.x += speed
+	marker.position.x += speed
 
 func _on_hitbox_body_entered(body):
 	destroy()
