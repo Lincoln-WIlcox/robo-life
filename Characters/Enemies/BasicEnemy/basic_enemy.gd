f812cc0cc29f654ba1890947418933e0d8107952
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-07-31 23:12:23 -0500

    enemy drops food on death

--- Characters/Enemies/BasicEnemy/basic_enemy.gd
+++ Characters/Enemies/BasicEnemy/basic_enemy.gd
@@ -1,19 +1,23 @@
 extends Node2D
 
 @onready var shoot_player_controller = $ShootPlayerController
+@onready var pickup_spawner = $GravityWalkOverPickupSpawner
 
 @export var target: Node2D
-@export var node_to_put_bullets_in: Node:
+@export var node_to_put_nodes_in: Node:
 	set(new_value):
-		node_to_put_bullets_in = new_value
+		node_to_put_nodes_in = new_value
 		if is_node_ready():
-			shoot_player_controller.node_to_put_bullets_in = node_to_put_bullets_in
+			shoot_player_controller.node_to_put_bullets_in = node_to_put_nodes_in
+			pickup_spawner.node_to_spawn_pickup_in = node_to_put_nodes_in
 
 func _ready():
-	shoot_player_controller.node_to_put_bullets_in = node_to_put_bullets_in
+	shoot_player_controller.node_to_put_bullets_in = node_to_put_nodes_in
+	pickup_spawner.node_to_spawn_pickup_in = node_to_put_nodes_in
 
 func _physics_process(delta):
 	shoot_player_controller.target = target.global_position
 
 func _on_overheater_max_heat_reached():
+	pickup_spawner.spawn()
 	queue_free()
--- Objects/GravityWalkOverPickupSpawner/gravity_walk_over_pickup_spawner.gd
+++ Objects/GravityWalkOverPickupSpawner/gravity_walk_over_pickup_spawner.gd
@@ -2,10 +2,17 @@ class_name GravityWalkOverPickupSpawner
 extends Node2D
 
 @export var gravity_walk_over_pickup_packed_scene: PackedScene
+@export var spawn_amount := 1
 @export var spawn_range_width := 10
 @export var node_to_spawn_pickup_in: Node
 
 func spawn():
+	var spawns_remaining = spawn_amount
+	while spawns_remaining > 0:
+		_spawn_one()
+		spawns_remaining -= 1
+
+func _spawn_one():
 	var gravity_walk_over_pickup = gravity_walk_over_pickup_packed_scene.instantiate()
 	node_to_spawn_pickup_in.add_child(gravity_walk_over_pickup)
 	gravity_walk_over_pickup.global_position = global_position
