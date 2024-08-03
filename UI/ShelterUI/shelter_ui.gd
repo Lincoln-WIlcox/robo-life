78d7c72f3f675f80acc8a79d33635d5599ab48cd
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-08-02 19:30:18 -0500

    end day button shows how much food is required to end day

--- UI/ShelterUI/shelter_ui.gd
+++ UI/ShelterUI/shelter_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const FOOD_TEXT = "food: "
+const END_DAY_BUTTON_TEXT = "end day "
 
 @onready var shelter_food_label = $ShelterContentVbox/ItemGridHbox/Interface1Vbox/CenterContainer/ItemGridInterface1FoodLabel
 @onready var player_food_label = $ShelterContentVbox/ItemGridHbox/VBoxContainer2/CenterContainer/ItemGridInterface2FoodLabel
@@ -24,10 +25,13 @@ signal transfer_food_pressed
 func _ready():
 	$ShelterContentVbox/ItemGridHbox/Interface1Vbox/ItemGridInterface1.item_grid = shelter_inventory.item_grid
 	$ShelterContentVbox/ItemGridHbox/VBoxContainer2/ItemGridInterface2.item_grid = player_inventory.item_grid
-	update_labels()
+	$ShelterContentVbox/EndDayButtonMargin/EndDayHbox/EndDayButton.text = END_DAY_BUTTON_TEXT + "(" + str(Utils.AMOUNT_OF_FOOD_TO_CONSUME) + ")"
 	
 	$ShelterContentVbox/EndDayButtonMargin/EndDayHbox/EndDayButton.pressed.connect(func(): end_day_pressed.emit())
 	$ShelterContentVbox/EndDayButtonMargin/EndDayHbox/TransferFoodButton.pressed.connect(func(): transfer_food_pressed.emit())
+	
+	update_labels()
+	update_end_day_disabled()
 
 func open_gui() -> void:
 	show()
@@ -38,3 +42,6 @@ func close_gui() -> void:
 func update_labels() -> void:
 	shelter_food_label.text = FOOD_TEXT + str(shelter_inventory.get_food()) + "/" + str(shelter_inventory.max_food)
 	player_food_label.text = FOOD_TEXT + str(player_inventory.get_food()) + "/" + str(player_inventory.max_food)
+
+func update_end_day_disabled() -> void:
+	$ShelterContentVbox/EndDayButtonMargin/EndDayHbox/EndDayButton.disabled = not shelter_inventory.get_food() + player_inventory.get_food() >= Utils.AMOUNT_OF_FOOD_TO_CONSUME
