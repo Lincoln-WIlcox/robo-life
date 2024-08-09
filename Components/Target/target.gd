cc69e78903307c318d5e4bc6d7c1384a15ff3316
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-08-09 16:44:11 -0500

    created target

---
+++ Components/Target/target.gd
@@ -0,0 +1,11 @@
+class_name Target
+extends Marker2D
+
+@export var following: Node2D:
+	set(new_value):
+		following = new_value
+		#ensures the node this is following gets its physics processed first
+		process_physics_priority = following.process_physics_priority - 1
+
+func _physics_process(delta):
+	global_position = following.global_position
