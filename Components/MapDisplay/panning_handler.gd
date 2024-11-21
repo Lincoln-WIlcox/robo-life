8d9ff4a2eafe8d0a8338bc04c74e45b14804f75e
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-11-20 18:07:45 -0600

    you can zoom in and out on the map

--- Components/MapDisplay/panning_handler.gd
+++ Components/MapDisplay/panning_handler.gd
@@ -1,8 +1,18 @@
 extends Node
 
+const MAX_SCALE = 0.9
+const MIN_SCALE = 0.1
+const SCALE_STEP = 0.05
+
 @export var scrollable_container: Node2D
 @export var map_control: Control
 
 func _input(event):
 	if event is InputEventMouseMotion and map_control.has_focus() and Input.is_action_pressed("map_pan"):
 		scrollable_container.position += event.relative
+	elif event.is_action_pressed("map_zoom_in"):
+		scrollable_container.scale.x = min(scrollable_container.scale.x + SCALE_STEP, MAX_SCALE)
+		scrollable_container.scale.y = scrollable_container.scale.x
+	elif event.is_action_pressed("map_zoom_out"):
+		scrollable_container.scale.x = max(scrollable_container.scale.x - SCALE_STEP, MIN_SCALE)
+		scrollable_container.scale.y = scrollable_container.scale.x
--- GenericClasses/map_data.gd
+++ GenericClasses/map_data.gd
@@ -3,10 +3,12 @@ extends Resource
 
 var _map_entities: Array[MapEntity]
 var _tilemap_polygons: Array[PackedVector2Array]
+var _bounding_box: Rect2
 
-func _init(map_entities: Array[MapEntity], tilemap_polygons: Array[PackedVector2Array]):
+func _init(map_entities: Array[MapEntity], tilemap_polygons: Array[PackedVector2Array], bounding_box: Rect2):
 	_map_entities = map_entities
 	_tilemap_polygons = tilemap_polygons
+	_bounding_box = bounding_box
 
 func get_tilemap_polygons():
 	return _tilemap_polygons
