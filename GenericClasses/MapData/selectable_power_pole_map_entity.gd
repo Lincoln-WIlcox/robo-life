63b4a1db5f1d35914cf4428b24fc73c396454db6
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-12-05 13:50:20 -0600

    power poles appear on map

--- GenericClasses/MapData/selectable_power_pole_map_entity.gd
+++ GenericClasses/MapData/selectable_power_pole_map_entity.gd
@@ -1,6 +1,9 @@
 class_name SelectablePowerPoleMapEntity
 extends MapScene
 
+@export var initial_position: Vector2
+
 func setup_scene() -> void:
 	super()
-	assert(instance is SelectablePowerPole)
+	assert(instance is SelectablePowerPole, "SelectablePowerPoleMapEntity given invalid packed scene")
+	instance.position = initial_position
