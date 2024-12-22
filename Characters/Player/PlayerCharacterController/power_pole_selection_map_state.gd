0ee35c3d551d0c2850b934ce74f2d4e5ae7aaf6a
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-12-22 12:24:25 -0600

    fixed several more unused parameter and unused signal warnings

--- Characters/Player/PlayerCharacterController/power_pole_selection_map_state.gd
+++ Characters/Player/PlayerCharacterController/power_pole_selection_map_state.gd
@@ -33,6 +33,7 @@ func _on_enviornment_query_system_queryable_added(added_queryable: QueryableEnti
 	if added_queryable.source_node is PowerPole:
 		_map_data.add_map_entity(added_queryable.source_node.power_pole_selection_map_entity)
 
+#when is this getting called?
 func _on_map_ui_power_pole_selected(power_pole: SelectablePowerPoleMapEntity) -> void:
 	state_ended.emit(none_state)
 	_map_ui.reset_selected_power_pole()
