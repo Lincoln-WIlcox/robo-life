045537b0090e6ea7e6840c3dbe83d08b51c7ceec
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2025-04-04 16:04:53 -0500

    power lines go dark when not enough power in a system

--- Components/PowerLineDrawer/power_line_drawer.gd
+++ Components/PowerLineDrawer/power_line_drawer.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 @export var line_width: float = 4
 @export var line_color: Color = Color(1,1,1)
+@export var no_power_color: Color = Color(1,1,1)
 
 var _redraw_queued := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PowerConnectionHandler.connections_changed.connect(redraw)
+	PowerConnectionHandler.connectors_state_changed.connect(redraw)
 	redraw()
 
 func redraw() -> void:
@@ -24,6 +26,9 @@ func redraw() -> void:
 			line.add_point(connection.power_connector_a.global_position)
 			line.add_point(connection.power_connector_b.global_position)
 			line.width = line_width
-			line.default_color = line_color
+			if connection.power_connector_a.powered:
+				line.default_color = line_color
+			else:
+				line.default_color = no_power_color
 			add_child(line)
 	_redraw_queued = false
