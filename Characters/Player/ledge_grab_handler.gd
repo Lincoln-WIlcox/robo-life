d9b1484ce4db962fe5344401fe7ecdc848b1c8e5
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2025-05-04 18:52:37 -0500

    falls off ledge if can no longer grab

--- Characters/Player/ledge_grab_handler.gd
+++ Characters/Player/ledge_grab_handler.gd
@@ -17,6 +17,9 @@ func _ready():
 func _on_grab_area_area_entered(area: Area2D, on_left_side: bool):
 	if area is Ledge and area.is_left_side != on_left_side:
 		ledge = area
-		var use_transform: Transform2D = grab_left.get_global_transform() if ledge.is_left_side else grab_right.get_global_transform()
-		if ledge.can_fit_on_grab(player_shape.shape, use_transform):
+		if can_grab_ledge():
 			ledge_grabbed.emit(ledge.is_left_side)
+
+func can_grab_ledge() -> bool:
+	var use_transform: Transform2D = grab_left.get_global_transform() if ledge.is_left_side else grab_right.get_global_transform()
+	return ledge.can_fit_on_grab(player_shape.shape, use_transform)
